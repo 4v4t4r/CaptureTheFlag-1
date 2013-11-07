0d5c2a8556aa63e4ef3c938484b19e99469446d6
Tomasz Szulc <tomasz.szulc@blstream.com>
2013-11-08 00:36:39 +0100

    Cleanups in CTFCredentialValidatorTests.

--- ctf-mob-ios/Capture The Flag/Capture The FlagTests/CTFCredentialValidatorTests.m
+++ ctf-mob-ios/Capture The Flag/Capture The FlagTests/CTFCredentialValidatorTests.m
@@ -17,23 +17,23 @@
 #pragma mark - CredentailTypeUsername
 - (void)testEmptyUsername
 {
-    XCTAssertTrue([self validUsername:@""] == ValidationEmptyField, @"");
+    XCTAssert([self validUsername:@""] == ValidationEmptyField, @"");
 }
 
 - (void)testUsernameLength
 {
-    XCTAssertTrue([self validUsername:@"login"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validUsername:@"loginn"] == ValidationOK, @"");
-    XCTAssertTrue([self validUsername:@"thisisverylonglogin"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validUsername:@"login"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validUsername:@"loginn"] == ValidationOK, @"");
+    XCTAssert([self validUsername:@"thisisverylonglogin"] == ValidationWrongCredentials, @"");
 }
 
 - (void)testUsernameCharacters
 {
-    XCTAssertTrue([self validUsername:@"login1"] == ValidationOK, @"");
-    XCTAssertTrue([self validUsername:@"123456"] == ValidationOK, @"");
-    XCTAssertTrue([self validUsername:@"_123456"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validUsername:@"_123d5_"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validUsername:@"valid!@__l"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validUsername:@"login1"] == ValidationOK, @"");
+    XCTAssert([self validUsername:@"123456"] == ValidationOK, @"");
+    XCTAssert([self validUsername:@"_123456"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validUsername:@"_123d5_"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validUsername:@"valid!@__l"] == ValidationWrongCredentials, @"");
 }
 
 - (ValidationResult)validUsername:(NSString *)username
@@ -45,23 +45,23 @@
 #pragma mark - CredentialTypePassword
 - (void)testEmptyPassword
 {
-    XCTAssertTrue([self validPassword:@""] == ValidationEmptyField, @"");
+    XCTAssert([self validPassword:@""] == ValidationEmptyField, @"");
 }
 
 - (void)testPasswordLength
 {
-    XCTAssertTrue([self validPassword:@"abc"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validPassword:@"ancdefgv"] == ValidationOK, @"");
-    XCTAssertTrue([self validPassword:@"123asd!@#%^|dggg66654"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validPassword:@"abc"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validPassword:@"ancdefgv"] == ValidationOK, @"");
+    XCTAssert([self validPassword:@"123asd!@#%^|dggg66654"] == ValidationWrongCredentials, @"");
 }
 
 - (void)testPasswordCharacters
 {
-    XCTAssertTrue([self validPassword:@"goodPass123!@#avc:)"] == ValidationOK, @"");
-    XCTAssertTrue([self validPassword:@"goodPassButWeak"] == ValidationOK, @"");
-    XCTAssertTrue([self validPassword:@"§1234567890-="] == ValidationOK, @"");
-    XCTAssertTrue([self validPassword:@"!@#$$%%^&*())"] == ValidationOK, @"");
-    XCTAssertTrue([self validPassword:@"this is not a password"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validPassword:@"goodPass123!@#avc:)"] == ValidationOK, @"");
+    XCTAssert([self validPassword:@"goodPassButWeak"] == ValidationOK, @"");
+    XCTAssert([self validPassword:@"§1234567890-="] == ValidationOK, @"");
+    XCTAssert([self validPassword:@"!@#$$%%^&*())"] == ValidationOK, @"");
+    XCTAssert([self validPassword:@"this is not a password"] == ValidationWrongCredentials, @"");
 }
 
 - (ValidationResult)validPassword:(NSString *)password
@@ -73,19 +73,19 @@
 #pragma mark - CredentialTypeEmail
 - (void)testEmptyEmail
 {
-    XCTAssertTrue([self validEmail:@""] == ValidationEmptyField, @"");
+    XCTAssert([self validEmail:@""] == ValidationEmptyField, @"");
 }
 
 - (void)testEmailCharacters
 {
-    XCTAssertTrue([self validEmail:@"abc@abcd.pl"] == ValidationOK, @"");
+    XCTAssert([self validEmail:@"abc@abcd.pl"] == ValidationOK, @"");
 //    XCTAssertTrue([self validEmail:@".%@.-.pl"] == ValidationOK, @""); /// this must not pass!
     
-    XCTAssertTrue([self validEmail:@"abcd@a.p"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validEmail:@"@.pl"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validEmail:@".pl"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validEmail:@"a.pl"] == ValidationWrongCredentials, @"");
-    XCTAssertTrue([self validEmail:@"a@pl"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validEmail:@"abcd@a.p"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validEmail:@"@.pl"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validEmail:@".pl"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validEmail:@"a.pl"] == ValidationWrongCredentials, @"");
+    XCTAssert([self validEmail:@"a@pl"] == ValidationWrongCredentials, @"");
 }
 
 - (ValidationResult)validEmail:(NSString *)email
