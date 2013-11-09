eab8dce46202cb1086af69abf676633f731165c6
Tomasz Szulc <tomasz.szulc@blstream.com>
2013-11-09 19:10:00 +0100

    Updated Podfile. Added RestKit and RestKit/Testing for Capture The FlagTests target.

---
+++ ctf-mob-ios/Capture The Flag/Capture The FlagTests/CTFRestKitTests.m
@@ -0,0 +1,21 @@
+//
+//  CTFRestKitTests.m
+//  Capture The Flag
+//
+//  Created by Tomasz Szulc on 09.11.2013.
+//  Copyright (c) 2013 Tomasz Szulc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <RestKit/RestKit.h>
+#import <RestKit/Testing.h>
+
+@interface CTFRestKitTests : XCTestCase
+
+@end
+
+@implementation CTFRestKitTests
+
+/// Added to simply check if RestKit and RestKit/Testing are visible for the Testing target
+
+@end
