2eb676ccb0d0dfeccfa5e3ff280fefaf239a8922
Tomasz Szulc <tomasz.szulc@blstream.com>
2013-11-18 20:04:51 +0100

    Added CTFUser class.

---
+++ ctf-mob-ios/Capture The Flag/Capture The Flag/CTFUser.h
@@ -0,0 +1,16 @@
+//
+//  CTFUser.h
+//  Capture The Flag
+//
+//  Created by Tomasz Szulc on 18.11.2013.
+//  Copyright (c) 2013 Tomasz Szulc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTFUser : NSObject
+
+@property (nonatomic) NSString *login;
+@property (nonatomic) NSString *token;
+
+@end
---
+++ ctf-mob-ios/Capture The Flag/Capture The Flag/CTFUser.m
@@ -0,0 +1,15 @@
+//
+//  CTFUser.m
+//  Capture The Flag
+//
+//  Created by Tomasz Szulc on 18.11.2013.
+//  Copyright (c) 2013 Tomasz Szulc. All rights reserved.
+//
+
+#import "CTFUser.h"
+
+@implementation CTFUser
+
+
+
+@end
