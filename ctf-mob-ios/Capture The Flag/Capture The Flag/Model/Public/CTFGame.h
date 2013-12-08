01b1a5c1a4c83f9a603091910a79d9481c12d2da
Tomasz Szulc <tomasz.szulc@blstream.com>
2013-12-08 14:21:41 +0100

    [CTFGUILD-11] Added enumerations to CTFGame.

--- ctf-mob-ios/Capture The Flag/Capture The Flag/Model/Public/CTFGame.h
+++ ctf-mob-ios/Capture The Flag/Capture The Flag/Model/Public/CTFGame.h
@@ -1,5 +1,19 @@
 #import "_CTFGame.h"
 
+typedef enum {
+    GameStatusUndefined = -1,
+    GameStatusInProgress,
+    GameStatusCreated,
+    GameStatusOnHold,
+    GameStatusCancelled
+} GameStatus;
+
+typedef enum {
+    GameTypeUndefined = -1,
+    GameTypeFrags,
+    GameTypeTime
+} GameType;
+
 @interface CTFGame : _CTFGame {}
 
 @end
