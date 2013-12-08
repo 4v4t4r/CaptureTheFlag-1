51346ab02937844d2fcf8bdd017c43db1cc53dee
Tomasz Szulc <tomasz.szulc@blstream.com>
2013-12-08 12:02:58 +0100

    [CTFGUILD-9] Added CTFItem class and cleanup in project structure.

---
+++ ctf-mob-ios/Capture The Flag/Capture The Flag/Model/CTFItem.h
@@ -0,0 +1,20 @@
+//
+//  CTFItem.h
+//  Capture The Flag
+//
+//  Created by Tomasz Szulc on 08.12.2013.
+//  Copyright (c) 2013 Tomasz Szulc. All rights reserved.
+//
+
+#import "CustomManagedObject.h"
+
+
+@interface CTFItem : CustomManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * desc;
+@property (nonatomic, retain) NSNumber * type;
+@property (nonatomic, retain) id location;
+@property (nonatomic, retain) NSNumber * value;
+
+@end
