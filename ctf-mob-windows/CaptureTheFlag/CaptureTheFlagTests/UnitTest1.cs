cab4e2ac725b48f3d37e19076b10894a455610ac
Piotr Marczycki <piotrekm44@o2.pl>
2014-01-14 23:11:29 +0100

    Adds initial visual studio solution

---
+++ ctf-mob-windows/CaptureTheFlag/CaptureTheFlagTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using CaptureTheFlag;
+
+namespace CaptureTheFlagTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
