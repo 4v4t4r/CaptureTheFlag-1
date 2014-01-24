2f7f6b117ecfdb6f8ef5e59b775297895fdaaac4
Tomasz Szulc <tomasz.szulc@blstream.com>
2014-01-24 21:52:43 +0100

    Removed Redundant code from CTFUser class.

--- ctf-mob-ios/Capture-The-Flag/Capture-The-Flag/Model/Public/CTFUser.h
+++ ctf-mob-ios/Capture-The-Flag/Capture-The-Flag/Model/Public/CTFUser.h
@@ -1,20 +1,5 @@
 #import "_CTFUser.h"
 
-@interface CTFUser : _CTFUser {}
-
-@end
-
-@interface CTFUser (CoreDataGeneratedAccessors)
-
-- (void)insertObject:(CTFCharacter *)value inCharactersAtIndex:(NSUInteger)idx;
-- (void)removeObjectFromCharactersAtIndex:(NSUInteger)idx;
-- (void)insertCharacters:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
-- (void)removeCharactersAtIndexes:(NSIndexSet *)indexes;
-- (void)replaceObjectInCharactersAtIndex:(NSUInteger)idx withObject:(CTFCharacter *)value;
-- (void)replaceCharactersAtIndexes:(NSIndexSet *)indexes withCharacters:(NSArray *)values;
-- (void)addCharactersObject:(CTFCharacter *)value;
-- (void)removeCharactersObject:(CTFCharacter *)value;
-- (void)addCharacters:(NSOrderedSet *)values;
-- (void)removeCharacters:(NSOrderedSet *)values;
+@interface CTFUser : _CTFUser
 
 @end
