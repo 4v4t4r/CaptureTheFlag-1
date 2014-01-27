a1b99097c842d49a4e60817d414f946515386219
Rafal <rafal@ubuntu.ubuntu-domain>
2014-01-27 22:23:07 +0100

    fix for initialization map

--- ctf-mob-android/CaptureTheFlag/CTFClient/src/main/java/com/blstream/ctfclient/activities/MapActivity.java
+++ ctf-mob-android/CaptureTheFlag/CTFClient/src/main/java/com/blstream/ctfclient/activities/MapActivity.java
@@ -2,17 +2,23 @@ package com.blstream.ctfclient.activities;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.blstream.ctfclient.CTF;
 import com.blstream.ctfclient.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Rafał Zadrożny on 1/27/14.
@@ -27,9 +33,43 @@ public class MapActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction().add(R.id.container, new MapFragment()).commit();
+        try {
+            initilizeMap();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+    }
+
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.map);
+            googleMap = ((MapFragment) fragment).getMap();
+
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        LatLng szczecin = new LatLng(53.4285, 14.5528);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(szczecin, 13));
+        googleMap.addMarker(new MarkerOptions().title("Capture the flag")
+                .snippet("In this city you will play this wonderful game.").position(szczecin));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkGooglePlayServices();
+        initilizeMap();
     }
 
     @Override
@@ -46,21 +86,15 @@ public class MapActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.menu_legalnotices:
-                String LicenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(
+                String licenseInfo = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(
                         getApplicationContext());
                 AlertDialog.Builder LicenseDialog = new AlertDialog.Builder(MapActivity.this);
                 LicenseDialog.setTitle("Legal Notices");
-                LicenseDialog.setMessage(LicenseInfo);
+                LicenseDialog.setMessage(licenseInfo);
                 LicenseDialog.show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        checkGooglePlayServices();
     }
 
     private void checkGooglePlayServices() {
@@ -73,7 +107,7 @@ public class MapActivity extends Activity {
                 GooglePlayServicesUtil.getErrorDialog(status, this, RQS_GooglePlayServices).show();
             }
         } catch (Exception e) {
-            Log.e("Error: GooglePlayServiceUtil: ", "" + e);
+            Log.e("Error: GooglePlayServiceUtil: ", "", e);
         }
     }
 }
