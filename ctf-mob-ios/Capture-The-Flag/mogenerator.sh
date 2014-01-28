e5e874319b41a4df86dd91ae8de0baad41061f7a
Tomasz Szulc <tomasz.szulc@blstream.com>
2014-01-28 07:49:10 +0100

    Mogenerator classes moved to /Externals and are in .gitignore.

--- ctf-mob-ios/Capture-The-Flag/mogenerator.sh
+++ ctf-mob-ios/Capture-The-Flag/mogenerator.sh
@@ -1,2 +1,2 @@
 cd Capture-The-Flag/Model/;
-/usr/bin/mogenerator -m Model.xcdatamodeld/Model.xcdatamodel --template-var arc=true --machine-dir Private/ --human-dir Public/
+/usr/bin/mogenerator -m Model.xcdatamodeld/Model.xcdatamodel --template-var arc=true --machine-dir ../../../Externals/Private/ --human-dir Public/
