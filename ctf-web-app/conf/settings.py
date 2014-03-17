a8d43681f00cd05beee36758c721324326c3a85e
KCU <kamil.chudy@blstream.com>
2014-03-17 15:12:34 +0100

    Changes haystack settings.

--- ctf-web-app/conf/settings.py
+++ ctf-web-app/conf/settings.py
@@ -150,13 +150,3 @@ LOGGING = {
         }
     }
 }
-
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
-    },
-}
-
-HAYSTACK_SIGNAL_PROCESSOR = 'apps.core.eshaystack.HaystackRealtimeSignalProcessor'
