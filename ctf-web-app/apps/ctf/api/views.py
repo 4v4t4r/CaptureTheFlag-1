57924144ff705eaf7b695ccb30f053ea30b8c268
mkr <michal.krawczak@blstream.com>
2014-03-26 14:27:10 +0100

    CTFGUILD-39: Add and remove player (character) to the selected game

--- ctf-web-app/apps/ctf/api/views.py
+++ ctf-web-app/apps/ctf/api/views.py
@@ -1,4 +1,6 @@
+import logging
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,6 +13,8 @@ from apps.ctf.api.serializers.maps import MapSerializer
 from apps.ctf.models import Map, Game, Item
 
 __author__ = 'mkr'
+
+logger = logging.getLogger("root")
 
 
 class MapViewSet(mixins.ModelPermissionsMixin,
@@ -60,6 +64,49 @@ class InGameLocation(APIView):
             user.lon = serializer.object.get('lon')
             user.save()
 
-            return Response('', status=status.HTTP_204_NO_CONTENT)
+            return Response('ok', status=status.HTTP_204_NO_CONTENT)
 
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class JoinToGame(APIView):
+    def patch(self, request, pk):
+        logger.debug("joining player to selected game...")
+
+        game = get_object_or_404(Game, pk=pk)
+        user = request.user
+        character = user.get_active_character()
+
+        if character is None:
+            return Response(data={"error": "An active character is not defined"}, status=status.HTTP_400_BAD_REQUEST)
+
+        logger.debug("character.user: %s", character.user)
+
+        is_user_already_exist = game.players.filter(user=user).exists()
+        logger.debug("is_user_already_exist: %s", is_user_already_exist)
+
+        if is_user_already_exist:
+            logger.info("User '%s' already joined into the game '%s'...", user.username, game.name)
+        else:
+            logger.info("User '%s' in character '%s' is joining into the game '%s'...", user.username, character.type, game.name)
+            game.players.add(character)
+            game.save()
+
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        logger.debug("removing player from selected game...")
+
+        game = get_object_or_404(Game, pk=pk)
+        user = request.user
+        character = user.get_active_character()
+
+        if character is None:
+            return Response(data={"error": "An active character is not defined"}, status=status.HTTP_400_BAD_REQUEST)
+
+        logger.info("User '%s' in character '%s' is removing from game '%s'...", user.username, character.type, game.name)
+
+        game.players.remove(character)
+        game.save()
+
+        return Response(status=status.HTTP_200_OK)
